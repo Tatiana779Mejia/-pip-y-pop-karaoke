dc9c6309589085c3a43b14e1c8c8b60bd20265a9
LAPT-CSH-32 <cshtd.computacion@gmail.com>
2025-03-06 09:28:02 -0600

    final

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //conexion  con HTML (selecciona relemenytos)
- 
+
 const songForm = document.getElementById('song_form');
 const playlist = document.getElementById('play_list');
- 
+
 //funcion para agregar una cancion ala lista
- 
+
 function addsong(songName, artistName, songUrl) {
     const listSong = document.createElement('li');
     listSong.innerHTML = `
@@ -12,24 +12,27 @@ function addsong(songName, artistName, songUrl) {
     - ${artistName}
     <a href="${songUrl}" target="_blank" >Reproducir</a>
     <button class="delete-btn">Eliminar </button>
- 
+
     `;
 //agregar al html
     playlist.appendChild(listSong);
- 
- 
-}    
- 
+    const deleteButton = listSong.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', ()=>{
+        listSong.remove();
+    })
+}   
+
+
 //manejo de envios de formulario (eventos)
- 
+
 songForm.addEventListener('submit',(event) =>{
     event.preventDefault();
- 
+
     //Obteber datos del formulario
     const songName = document.getElementById("song_name").value ;
     const artistName =document.getElementById("artist_name"). value;
     const songUrl =document.getElementById("url_song"). value;
     addsong(songName,artistName,songUrl);
- 
+
     songForm.reset()
 })
